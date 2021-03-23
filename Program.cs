3334c1fd8c092bc92dbac5e0293a77433db23bd8
Isaac <isaacgeazy@gmail.com>
2021-03-23 13:50:34 -0300

    Atividade HoraSistema

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Datas_e_pacotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DateTime agora = DateTime.Now;
+
+           Console.WriteLine($"{agora}");
+        }
+    }
+}
